1cd7942cf93b0ec64be59266f34cb140174c4265
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-29 22:46:06 -0700

    add join activity with button on main activity to link

---
+++ app/src/main/java/com/example/guest/socialnetworking/JoinActivity.java
@@ -0,0 +1,8 @@
+package com.example.guest.socialnetworking;
+
+/**
+ * Created by storm on 5/29/2017.
+ */
+
+public class JoinActivity {
+}
--- app/src/main/java/com/example/guest/socialnetworking/MainActivity.java
+++ app/src/main/java/com/example/guest/socialnetworking/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.guest.socialnetworking;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
     private TextView mBannerText;
     private TextView mBannerText2;
+    @Bind(R.id.joinButton) Button mjoinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,9 +23,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         mBannerText = (TextView) findViewById(R.id.bannerText);
         mBannerText2 = (TextView) findViewById(R.id.bannerText2);
-
+        ButterKnife.bind(this);
         Typeface crash = Typeface.createFromAsset(getAssets(), "fonts/crash.ttf");
         mBannerText.setTypeface(crash);
         mBannerText2.setTypeface(crash);
+        mjoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, JoinActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
