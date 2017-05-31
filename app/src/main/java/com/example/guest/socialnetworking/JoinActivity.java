9e9e821636d33f90bbe9f65f99b5d5ad2f69ab17
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-30 17:37:02 -0700

    add function and image to landing activity, links to explore, about, and contact

--- app/src/main/java/com/example/guest/socialnetworking/JoinActivity.java
+++ app/src/main/java/com/example/guest/socialnetworking/JoinActivity.java
@@ -1,13 +1,29 @@
 package com.example.guest.socialnetworking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class JoinActivity extends AppCompatActivity {
+    @Bind(R.id.joinButton) Button mjoinButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join2);
+        ButterKnife.bind(this);
+
+        mjoinButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(JoinActivity.this, Landing.class);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/guest/socialnetworking/Landing.java
+++ app/src/main/java/com/example/guest/socialnetworking/Landing.java
@@ -1,13 +1,45 @@
 package com.example.guest.socialnetworking;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class Landing extends AppCompatActivity {
+    @Bind(R.id.contactButton) Button mcontactButton;
+    @Bind(R.id.aboutButton) Button maboutButton;
+    @Bind(R.id.exploreButton) Button mexploreButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        ButterKnife.bind(this);
+
+        maboutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Landing.this, About.class);
+                startActivity(intent);
+            }
+        });
+        mcontactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Landing.this, Contact.class);
+                startActivity(intent);
+            }
+        });
+        mexploreButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Landing.this, ExploreActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
