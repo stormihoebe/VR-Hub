55c8ea578beaad8952adb52431146af8cd4359eb
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-30 17:14:36 -0700

    add landing page

--- app/src/main/java/com/example/guest/socialnetworking/Landing.java
+++ app/src/main/java/com/example/guest/socialnetworking/Landing.java
@@ -3,11 +3,11 @@ package com.example.guest.socialnetworking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class JoinActivity extends AppCompatActivity {
+public class Landing extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_join2);
+        setContentView(R.layout.activity_landing);
     }
 }
--- app/src/main/java/com/example/guest/socialnetworking/MainActivity.java
+++ app/src/main/java/com/example/guest/socialnetworking/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.guest.socialnetworking;
 
 import android.content.Intent;
 import android.graphics.Typeface;
-import android.support.v4.widget.TextViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
