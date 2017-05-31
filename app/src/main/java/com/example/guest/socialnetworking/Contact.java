6560303546a4db77212ba16552b13131609e3a82
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-05-31 08:36:56 -0700

    add toast to contact

--- app/src/main/java/com/example/guest/socialnetworking/Contact.java
+++ app/src/main/java/com/example/guest/socialnetworking/Contact.java
@@ -2,12 +2,26 @@ package com.example.guest.socialnetworking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class Contact extends AppCompatActivity {
+    @Bind(R.id.ContactSubmitButton) Button mcontactSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+        ButterKnife.bind(this);
+        mcontactSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Contact.this, "Thank you!", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
