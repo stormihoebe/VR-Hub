8400c70b28c8932f7d51f341b8ac898cd5477f26
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-01 10:13:40 -0700

    stage app for API calls

---
+++ app/src/main/java/com/example/guest/socialnetworking/Constants.java
@@ -0,0 +1,12 @@
+package com.example.guest.socialnetworking;
+
+/**
+ * Created by Guest on 6/1/17.
+ */
+
+public class Constants {
+    public static final String TWITTER_CONSUMER_KEY = BuildConfig.TWITTER_CONSUMER_KEY;
+    public static final String TWITTER_CONSUMER_SECRET = BuildConfig.TWITTER_CONSUMER_SECRET;
+    public static final String TWITTER_TOKEN = BuildConfig.TWITTER_TOKEN;
+    public static final String TWITTER_TOKEN_SECRET = BuildConfig.TWITTER_TOKEN_SECRET;
+}
