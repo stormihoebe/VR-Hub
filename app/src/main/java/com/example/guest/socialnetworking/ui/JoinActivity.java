5d78494f71c4f4686213e4a60a3caa77971e7565
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-08 13:54:32 -0700

    add function to join activity to create user account and login new user

--- app/src/main/java/com/example/guest/socialnetworking/ui/JoinActivity.java
+++ app/src/main/java/com/example/guest/socialnetworking/ui/JoinActivity.java
@@ -1,8 +1,10 @@
 package com.example.guest.socialnetworking.ui;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,12 +12,18 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.guest.socialnetworking.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class JoinActivity extends AppCompatActivity implements View.OnClickListener {
+    public static final String TAG = JoinActivity.class.getSimpleName();
+
     @Bind(R.id.joinButton) Button mjoinButton;
     @Bind(R.id.NewUserName) EditText muserName;
     @Bind(R.id.NewUserEmail) EditText muserEmail;
@@ -24,12 +32,13 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
 
     private FirebaseAuth mAuth;
 
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join2);
         ButterKnife.bind(this);
+        createAuthStateListener();
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -40,6 +49,7 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View view) {
         if (view == mtoLogin) {
             Intent intent = new Intent(JoinActivity.this, Login.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
         }
@@ -47,10 +57,55 @@ public class JoinActivity extends AppCompatActivity implements View.OnClickListe
             createNewUser();
         }
     }
+
+    private void createAuthStateListener() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                final FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(JoinActivity.this, Landing.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+
+        };
+    }
+
     public void createNewUser() {
         final String name = muserName.getText().toString().trim();
-        final String username = muserEmail.getText().toString().trim();
+        final String email = muserEmail.getText().toString().trim();
         final String password = muserPassword.getText().toString().trim();
-// TODO: 6/8/17  FINISH THIS CREATE NEW USER IN
+
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(JoinActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 }
