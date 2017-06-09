4839e4c100373854b6b30a35ff46ceb78244c610
Stormi Hoebelheinrich <stormihoebe@gmail.com>
2017-06-09 12:10:31 -0700

    adjusted todo list

--- app/src/main/java/com/example/guest/socialnetworking/models/Message.java
+++ app/src/main/java/com/example/guest/socialnetworking/models/Message.java
@@ -6,8 +6,8 @@ import org.parceler.Parcel;
  * Created by Guest on 6/8/17.
  */
 // TODO: 6/8/17 Use SharedPreferences to save an important piece of data in your app. (save username?)
-// TODO: 6/8/17 Update README 
 // TODO: 6/8/17  Implicit Intent (send message in contact)
+// TODO: 6/9/2017 Remove Twitter details from sign in 
 
 @Parcel
 public class Message {
